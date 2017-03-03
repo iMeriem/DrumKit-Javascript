b5ac387624b96a6caa6cb8c567d13f41b9fc1b27
iMeriem <meriemzaid@gmail.com>
2017-03-03 01:04:23 +0000

    Initial Commit.

---
+++ scripts.js
@@ -0,0 +1,23 @@
+const keys = document.querySelectorAll('.key');
+
+function playSound(event) {
+  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+  
+  if (!audio)
+    return;
+  
+  audio.currentTime = 0;
+  audio.play();
+  key.classList.add('playing');
+}
+
+function removeTransition(event) {
+  if (event.propertyName !== 'transform')
+    return;
+  
+  this.classList.remove('playing');
+}
+
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
